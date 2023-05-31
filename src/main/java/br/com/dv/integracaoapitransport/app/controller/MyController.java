362c92734040a2a5cce7667c4028e56e1e3f9ecf
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-31 10:53:32 -0300

    fix controller annotation

--- src/main/java/br/com/dv/integracaoapitransport/app/controller/MyController.java
+++ src/main/java/br/com/dv/integracaoapitransport/app/controller/MyController.java
@@ -4,10 +4,14 @@ import br.com.dv.integracaoapitransport.app.model.fastcommerce.request.Fastcomme
 import br.com.dv.integracaoapitransport.app.model.fastcommerce.response.FastcommerceResponse;
 import br.com.dv.integracaoapitransport.app.service.MainService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MyController {
 
