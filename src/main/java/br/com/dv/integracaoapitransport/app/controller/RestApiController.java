e4efd967851cecbb38391114109a97f0e2319f53
Douglas Vieira <douglasdotv@hotmail.com>
2023-06-03 16:05:49 -0300

    Handle quote in json payload

--- src/main/java/br/com/dv/integracaoapitransport/app/controller/RestApiController.java
+++ src/main/java/br/com/dv/integracaoapitransport/app/controller/RestApiController.java
@@ -3,6 +3,8 @@ package br.com.dv.integracaoapitransport.app.controller;
 import br.com.dv.integracaoapitransport.app.model.fastcommerce.request.FastcommerceRequest;
 import br.com.dv.integracaoapitransport.app.model.fastcommerce.response.FastcommerceResponse;
 import br.com.dv.integracaoapitransport.app.service.MainService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestApiController {
 
     private final MainService mainService;
+    private final ObjectMapper objectMapper;
 
-    public RestApiController(MainService mainService) {
+    public RestApiController(MainService mainService, ObjectMapper objectMapper) {
         this.mainService = mainService;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping
-    public ResponseEntity<FastcommerceResponse> handleRequest(@RequestBody FastcommerceRequest fastcommerceRequest) {
-        return ResponseEntity.ok(mainService.handleRequest(fastcommerceRequest));
+    public ResponseEntity<FastcommerceResponse> handleRequest(@RequestBody String payload) {
+        String json = payload.trim();
+        if (json.startsWith("quote")) {
+            json = json.substring(6);
+        }
+
+        FastcommerceRequest fcRequest;
+        try {
+            fcRequest = objectMapper.readValue(json, FastcommerceRequest.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(mainService.handleRequest(fcRequest));
     }
 
 }
